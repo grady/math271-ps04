7b629ada1f38a04254b30fa8ddf6bbd5c2327fb0
Grady Weyenberg <gradysw@hawaii.edu>
2024-01-28 16:28:16 -1000

    updating autograding scripts

--- tests.R
+++ tests.R
@@ -1,17 +1,22 @@
-## execute all code in the solution file
-try(knitr::knit(text=readLines('PS04_solution.Rmd')))
-
-test_that("Data import", {
-  expect_equal(dim(raw_solar), c(82, 9139))
+test_that("<5>Solution file exists and knits",{
+  expect_true(file.exists("solution.Rmd"))
+  suppressWarnings(expect_error(knitr::knit("solution.Rmd", quiet = TRUE, envir=globalenv()), NA))
 })
 
-test_that("Pivot and rename", {
-  expect_equal(nrow(solar), 241502)
-  expect_true(all(c("name", "id", "lon", "lat", "elev", "date", "irradiance") 
-                  %in% names(solar)))
+test_that("<1> Data import", {
+  expect_error(expect_equal(dim(raw_solar), c(82, 9139)),NA)
 })
 
-test_that("Dates",{
-  expect_s3_class(solar$date, "Date")
-  expect_s3_class(solar$month, "factor")
+test_that("<2> Pivot and rename", {
+  expect_error(expect_equal(nrow(solar), 241502) , NA)
+  expect_error(expect_true(all(c("name", "id", "lon", "lat", "elev", "date", "irradiance") 
+                               %in% names(solar))) , NA)
+})
+
+test_that("<2> Dallas summary data frame", {
+  expect_error(expect_s3_class(solar$date, "Date"), NA)
+  expect_error(expect_s3_class(solar$month, "factor"), NA)
+  #expect_error( , NA)
+  #expect_error( , NA)
+  
 })
